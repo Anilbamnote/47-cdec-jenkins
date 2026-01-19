e676da71f60b07acfcba09cc70acffdd5bfe67b9
Anil Bamnote <anilbamnote21@gmail.com>
2026-01-19 14:36:10 +0530

    pull-stage-added

---
+++ pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('git-pull-stage') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+         stage('build-stage') {
+            steps {
+                echo 'code build sucessfully'
+            }
+        }
+         stage('tesr-stage') {
+            steps {
+                echo 'code test sucessfully'
+            }
+        }
+         stage('deploy-stage') {
+            steps {
+                echo 'code deploy sucessfully'
+            }
+        }
+    }
+}
